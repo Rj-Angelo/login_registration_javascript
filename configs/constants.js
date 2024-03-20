bd36c7bb7aedc20d208852b1350fd5afdac38adc
Rj-Angelo <rj.arielangelo@gmail.com>
2024-03-20 16:03:30 +0800

    Update constants.js

--- configs/constants.js
+++ configs/constants.js
@@ -3,7 +3,7 @@ const constant = {};
 constant.PORT = 3000;
 
 constant.DATABASE = {
-    host: "localhost",
+    host: "amp-db.ch6aewwyy70e.ap-southeast-1.rds.amazonaws.com",
     user: "admin",
     password: "w8U3b9Zcfgby2EhZKiTB",
     database: "wall",
